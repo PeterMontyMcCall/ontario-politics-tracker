b0d22d334aeb42172b841d1287f70cc66b173460
PeterMontyMcCall <pdminh2004@gmail.com>
2025-07-15 00:03:32 -0400

    Fix cron schedular

--- src-server/scheduler/scheduler.js
+++ src-server/scheduler/scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { refreshArticles } = require('../services/articleRefresher');
 
 // Schedule to run every 15 minutes
-cron.schedule('* */3 * * *', async () => {
+cron.schedule('0 */3 * * *', async () => {
     console.log('Refreshing articles...');
     try {
         await refreshArticles();
